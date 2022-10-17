4768605e4c0a4d06568afc06f4e431a9ba73a9e1
L <lece619@gmail.com>
2022-10-17 14:54:34 +0900

    Initial commit

---
+++ src/main/java/com/jino/jinoapi/JinoapiApplication.java
@@ -0,0 +1,13 @@
+package com.jino.jinoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinoapiApplication.class, args);
+	}
+
+}
